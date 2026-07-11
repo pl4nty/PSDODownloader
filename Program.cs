906d52f96532424da58ed9b52737dda805f8f084
Tom Plant <tom@tplant.com.au>
2026-07-11 14:58:58 +1000

    fix: default to IME's caller ID so downloads are cached
    Fixes #13

--- Program.cs
+++ Program.cs
@@ -212,7 +212,7 @@ namespace DODownloader
 
             options.SetRangesIfEmpty();
 
-            var factory = new DODownloadFactory(Caller ?? "PSDODownloader");
+            var factory = new DODownloadFactory(Caller ?? "IntuneAppDownload");
             var file = new DOFile(options.Url, ContentId);
 
             DODownload download = null;
